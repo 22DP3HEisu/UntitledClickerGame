16afa40f2c627ffed03e91be1203f33590f14077
22DP3RZelc <rz00754@rvt.lv>
2025-09-09 22:13:17 +0300

    Changed some major things

--- Assets/C#/Clicker.cs
+++ Assets/C#/Clicker.cs
@@ -4,7 +4,17 @@ using UnityEngine.InputSystem;
 public class Clicker : MonoBehaviour
 {
     public Transform targetTransform;
+    public Transform HorseSprite;
     public ClickPopupSpawner popupSpawner;
+
+    private Vector3 originalScale;
+    public float scaleUpFactor = 1.2f;
+    public float scaleDuration = 0.1f;
+
+    void Start()
+    {
+        originalScale = HorseSprite.localScale;
+    }
 
     void Update()
     {
@@ -15,9 +25,18 @@ public class Clicker : MonoBehaviour
 
             if (hit != null && hit.transform == targetTransform)
             {
+                // Increase sprite size
+                HorseSprite.localScale = originalScale * scaleUpFactor;
+                StartCoroutine(ResetScale());
+
                 ClickManager.Instance.AddClicks(1);
                 popupSpawner.SpawnPopup(mousePos, "+1");
             }
         }
+    }
+    private System.Collections.IEnumerator ResetScale()
+    {
+        yield return new WaitForSeconds(scaleDuration);
+        HorseSprite.localScale = originalScale;
     }
 }
